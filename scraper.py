0b78e35e6def4195ed15bbcc81e78d11843910c7
ashley-alt <a.olubanjo1@gmail.com>
2021-10-31 13:15:55 +0000

    final commit

--- scraper.py
+++ scraper.py
@@ -7,13 +7,6 @@ URL = 'https://www.amazon.co.uk/LG-32UN88A-Monitor-IPS-3840x2160-Ergonomic/dp/B0
 headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36'}
 
 
-def send_mail():
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-
-    
 
 
 def check_price():
@@ -29,6 +22,28 @@ def check_price():
         send_mail()
 
 
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+server.login('a.omotayo.olubanjo1@gmail.com',)
+
+subject = 'Drop in price'
+body = "Check the amazon link: https://www.amazon.co.uk/LG-32UN88A-Monitor-IPS-3840x2160-Ergonomic/dp/B088LVN3TW/ref=sr_1_3?crid=1PM3GCBVKGTZD&dchild=1&keywords=31.5%27%27%2BUltraFine%E2%84%A2%2BUHD%2B4K%2BErgo%2BIPS%2BMonitor%2Bwith%2BUSB%2BType-C%E2%84%A2&qid=1635681403&sprefix=31.5%2Bultrafine%2Buhd%2B4k%2Bergo%2Bips%2Bmonitor%2Bwith%2Busb%2Btype-c%2B%2Caps%2C283&sr=8-3&th=1"
+
+message = f"Subject: {subject}\n\n{body}"
+
+server.sendmail(
+    'asheley11@hotmail.co.uk'
+    'a.omotayo.olubanjo1@gmail.com'
+    message
+)
+print('Hey, email has been sent')
+
+server.quit()
 
 
 
